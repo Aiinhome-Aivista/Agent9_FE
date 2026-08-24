28d6ec42a155c4528c1ca50dbf2725f53c4752cf
Sathi Chakraborti <chakrabortisathi111@gmail.com>
2026-08-24 18:36:11 +0530

    deply

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_API_URL || "";
+const BASE = import.meta.env.VITE_API_URL || "http://187.127.163.17:3035";
 
 const api = axios.create({
   baseURL: BASE,
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://0.0.0.0:3004", //http://122.163.121.176:3004
+        target: process.env.VITE_API_URL || "http://187.127.163.17:3035",
         changeOrigin: true,
         secure: false,
       },
